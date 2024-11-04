1f126098a962bd44e2b897795bf9e4ae88bdbff6
Rajani45 <163117857+Rajani45@users.noreply.github.com>
2024-11-04 08:14:40 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,8 +12,15 @@ provider "commercetools" {
   # client_id     = "OJ0FbNgxAEnhEOW4VMTD33UM"
   # client_secret = "pT4hZceyb29ZA6a8CeCJMlqgMyKSVE9F"
   # project_key   = "naga-demo-project1"
-  scopes          =  "manage_project:hcl-devops view_api_clients:hcl-devops manage_api_clients:hcl-devops view_audit_log:hcl-devops"
+  #scopes          =  "manage_project:hcl-devops view_api_clients:hcl-devops manage_api_clients:hcl-devops view_audit_log:hcl-devops"
   # api_url       = "https://api.us-central1.gcp.commercetools.com"
-  token_url     = "http://auth.us-central1.gcp.commercetools.com"  
+  #token_url     = "http://auth.us-central1.gcp.commercetools.com"  
 # oauth_url="https://auth.us-central1.gcp.commercetools.com"
+
+     project= "hcl-devops"
+    client_id= "WinjyxPqKX-6jVyMaSg3dNhP"
+    client_secret= "YIS4PFFvhmmX2Okf2YIu9ses7XXnxgUv"
+    oauth_url= "https://auth.us-central1.gcp.commercetools.com"
+    api_url= "https://api.us-central1.gcp.commercetools.com"
+    scope:= "manage_project:hcl-devops view_api_clients:hcl-devops manage_api_clients:hcl-devops view_audit_log:hcl-devops"
 }
