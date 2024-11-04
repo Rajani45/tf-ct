9aa2e9017d16d8b3a217dd440f6dbe685457fa70
Rajani45 <163117857+Rajani45@users.noreply.github.com>
2024-11-04 07:49:14 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,7 +12,7 @@ provider "commercetools" {
   # client_id     = "OJ0FbNgxAEnhEOW4VMTD33UM"
   # client_secret = "pT4hZceyb29ZA6a8CeCJMlqgMyKSVE9F"
   # project_key   = "naga-demo-project1"
-  scopes        =  "manage_project:devops manage_api_clients:devops view_api_clients:devops"
+  scopes          =  "manage_project:hcl-devops view_api_clients:hcl-devops manage_api_clients:hcl-devops view_audit_log:hcl-devops"
   # api_url       = "https://api.us-central1.gcp.commercetools.com"
   # token_url     = "https://auth.us-central1.gcp.commercetools.com"  
 }
