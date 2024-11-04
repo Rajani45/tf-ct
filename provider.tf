aeff5ebe97babbc6992391cd48cb3b46babf6fb2
Rajani45 <163117857+Rajani45@users.noreply.github.com>
2024-11-04 07:32:09 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,7 +12,7 @@ provider "commercetools" {
   # client_id     = "OJ0FbNgxAEnhEOW4VMTD33UM"
   # client_secret = "pT4hZceyb29ZA6a8CeCJMlqgMyKSVE9F"
   # project_key   = "naga-demo-project1"
-  scopes        = "manage_project:naga-demo-project1 manage_api_clients:naga-demo-project1 view_api_clients:naga-demo-project1 view_audit_log:naga-demo-project1"
+  scopes        =  "manage_project:devops manage_api_clients:devops view_api_clients:devops"
   # api_url       = "https://api.us-central1.gcp.commercetools.com"
-  token_url     = "https://auth.us-central1.gcp.commercetools.com"  
+  # token_url     = "https://auth.us-central1.gcp.commercetools.com"  
 }
